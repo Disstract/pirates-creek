173091926d48a7e04bd3c8b2c18aa74f5e5774b7
iCraft85 <aryanb.in@gmail.com>
2021-12-07 18:21:15 +0800

    Added try catch to fix index error

--- src/urlTamperable/crackhub_Filter.py
+++ src/urlTamperable/crackhub_Filter.py
@@ -7,12 +7,18 @@ session = HTMLSession()
 r = session.get("https://crackhub.site/?s="+search)
 
 titles = r.html.find('.entry-title')
-link = r.html.find('.entry-title a')
+try:
+    link = r.html.find('.entry-title a')
+except:
+    pass
 
 print("\n-----------!!Crackhub!!-----------")
 for i in range(0, len(titles)):
     print(titles[i].text)
-    print(link[i].attrs['href'])
+    try:
+        print(link[i].attrs['href'])
+    except:
+        pass
     print("------------------------------")
     time.sleep(0.05)
     

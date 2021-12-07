b943cf4b76c6aaaef33ec0be779379f8903d3341
iCraft85 <aryanb.in@gmail.com>
2021-12-07 22:08:02 +0800

    e

---
+++ src/urlTamperable/csrinruFilter.py
@@ -0,0 +1,13 @@
+#https://cs.rin.ru/forum/search.php?keywords=enter&terms=any&author=&sc=1&sf=titleonly&sk=t&sd=d&sr=topics&st=0&ch=300&t=0&submit=Search
+from requests_html import HTMLSession
+import time
+
+
+search = str(input("Enter Search: "))
+# def csrinruSearch(search):
+session = HTMLSession()
+r = session.get("https://cs.rin.ru/forum/search.php?keywords="+search+"&terms=any&author=&sc=1&sf=titleonly&sk=t&sd=d&sr=topics&st=0&ch=300&t=0&submit=Search")
+
+titles = r.html.find('.topictitle')
+
+print(titles)

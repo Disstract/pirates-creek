075218ddd9420ccba67880594200ed5c3cb7f13d
iCraft85 <aryanb.in@gmail.com>
2021-12-07 22:27:41 +0800

    gog games done

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import numpy 
 import requests
 
-from urlTamperable import crackhubScene_Filter, crackhub_Filter, gloadFilter
+from urlTamperable import crackhubScene_Filter, crackhub_Filter, gloadFilter, gogGamesFilter
 
 search = str(input("Enter Search: "))
 
 crackhub_Filter.crackhubSearch(search)
 crackhubScene_Filter.crackhubSceneSearch(search)
 gloadFilter.gloadSearch(search)
+gogGamesFilter.gogGamesSearch(search)
---
+++ src/urlTamperable/gogGamesFilter.py
@@ -0,0 +1,23 @@
+#https://gog-games.com/search/enter
+from requests_html import HTMLSession
+import time
+
+def gogGamesSearch(search):
+    session = HTMLSession()
+    r = session.get("https://gog-games.com/search/"+search)
+
+    titles = r.html.find('.title')
+    link = r.html.find('.block')
+
+    print("\n-----------!!GOG Games!!-----------")
+    if titles == []:
+        print("Nothing Found")
+        print("------------------------------")
+    else:
+        for i in range(0, len(titles)):
+            
+            print(titles[i].text)
+            print("https://gog-games.com"+link[i].attrs['href'])
+
+            print("------------------------------")
+            time.sleep(0.05)

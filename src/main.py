2d992459f93f074818dd1e4194e9fe767e2e4570
iCraft85 <aryanb.in@gmail.com>
2021-12-07 22:17:51 +0800

    added gload filter

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import numpy 
 import requests
 
-from urlTamperable import crackhubScene_Filter, crackhub_Filter
+from urlTamperable import crackhubScene_Filter, crackhub_Filter, gloadFilter
 
 search = str(input("Enter Search: "))
 
 crackhub_Filter.crackhubSearch(search)
 crackhubScene_Filter.crackhubSceneSearch(search)
+gloadFilter.gloadSearch(search)

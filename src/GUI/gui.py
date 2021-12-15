98eed0653a59405b5ed3d69f2b4b4724eb2298b8
iCraft85 <aryanb.in@gmail.com>
2021-12-15 14:56:32 +0800

    commit

--- src/GUI/gui.py
+++ src/GUI/gui.py
@@ -1,12 +1,11 @@
 from tkinter import *
-
-class Window(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.master = master
+from tkinter import ttk
 
 root = Tk()
-app = Window(root)
 
-root.wm_title("Pirate Ship")
+frame = ttk.Frame(root, padding=10)
+frame.grid()
+
+ttk.Entry
+
 root.mainloop()

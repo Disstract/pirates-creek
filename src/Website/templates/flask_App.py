c385ddadfc43f3db3f43669a3dfe88f3709b7731
mystery_in_a_hoodie <53719487+ACE-OF-DIAMONDS@users.noreply.github.com>
2021-12-16 13:31:18 +0800

    updated file structure so that the jinja2 rnd works

---
+++ src/Website/templates/flask_App.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='template')
+
+@app.route('/')
+def main():
+    return render_template("website.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+    '''
+    $ export FLASK_APP=flask_App.py
+    $ flask run
+    or just run thur idle if it doesn't work
+        '''
+    

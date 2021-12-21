19b5fc16f4380759f87ae32ebdce3d21d7cd6c50
iCraft85 <aryanb.in@gmail.com>
2021-12-21 20:54:56 +0530

    e

--- src/urlTamperable/DirectSites/crackhub_Filter.py
+++ src/urlTamperable/DirectSites/crackhub_Filter.py
@@ -2,27 +2,27 @@ from requests_html import HTMLSession
 import time
 
 def crackhubSearch(search):
-	results = []
-	try:
-		session = HTMLSession()
-		r = session.get("https://crackhub.site/?s="+search)
+    results = []
+    try:
+        session = HTMLSession()
+        r = session.get("https://crackhub.site/?s="+search)
 
-		titles = r.html.find('.entry-title')
-		try:
-			link = r.html.find('.entry-title a')
-		except:
-			pass
-        
-		for i in range(0, len(titles)):
-			results.append(titles[i].text)
-			try:
-				results.append(link[i].attrs['href'])
-			except:
-				pass
-		if results == []:
-			results.append("Nothing Found")
-		return(results)
-	except:
-		return("Crackhub: Connection Failed!")
-    
+        titles = r.html.find('.entry-title')
+        try:
+            link = r.html.find('.entry-title a')
+        except:
+            pass
+
+        for i in range(0, len(titles)):
+            results.append(titles[i].text)
+            try:
+                results.append(link[i].attrs['href'])
+            except:
+                pass
+        if results == []:
+            results.append("Nothing Found")
+        return(results)
+    except:
+        return("Crackhub: Connection Failed!")
+
 

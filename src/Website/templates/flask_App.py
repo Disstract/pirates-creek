3a148b2e10fcbaf0be5d5d2d41246d55a723fe53
mystery_in_a_hoodie <53719487+ACE-OF-DIAMONDS@users.noreply.github.com>
2021-12-21 23:23:44 +0800

    added search results file

--- src/Website/templates/flask_App.py
+++ src/Website/templates/flask_App.py
@@ -5,6 +5,9 @@ app = Flask(__name__, template_folder='template')
 def main():
     return render_template("website.html")
 
+def result():
+    return render_template("searchres.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
     app.run(host='0.0.0.0', port=5000, debug=True)

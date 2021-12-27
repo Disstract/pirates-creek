56fd923660c46338f85f3a6ac2078636a9c1165e
mystery_in_a_hoodie <53719487+ACE-OF-DIAMONDS@users.noreply.github.com>
2021-12-28 05:37:45 +0900

    pyc files

--- src/creek_pirate/creek_pirate/Website/templates/main.py
+++ src/creek_pirate/creek_pirate/Website/templates/main.py
@@ -3,34 +3,34 @@ from urlTamperable.Repacks import chovkaRepackFilter, fitgirlRepackFilter
 
 search = str(input("Enter Search: "))
 
-# crackhubResults = crackhub_Filter.crackhubSearch(search)
-# print(" ")
-# print(crackhubResults)
+crackhubResults = crackhub_Filter.crackhubSearch(search)
+print(" ")
+print(crackhubResults)
 
 
 crackhubSceneResults = crackhubScene_Filter.crackhubSceneSearch(search)
 print(" ")
 print(crackhubSceneResults)
 
-# gloadResults = gloadFilter.gloadSearch(search)
-# print(" ")
-# print(gloadResults)
+gloadResults = gloadFilter.gloadSearch(search)
+print(" ")
+print(gloadResults)
 
-# gogGamesResults = gogGamesFilter.gogGamesSearch(search)
-# print(" ")
-# print(gogGamesResults)
+gogGamesResults = gogGamesFilter.gogGamesSearch(search)
+print(" ")
+print(gogGamesResults)
 
-# #ovaGamesResults = ovagamesFilter.ovaGamesSearch(search)
-# #print(" ")
-# #print(ovaGamesResults)
+ovaGamesResults = ovagamesFilter.ovaGamesSearch(search)
+print(" ")
+print(ovaGamesResults)
 
-# chovkaRepackResults = chovkaRepackFilter.chovkaSearch(search)
-# print(" ")
-# print(chovkaRepackResults)
+chovkaRepackResults = chovkaRepackFilter.chovkaSearch(search)
+print(" ")
+print(chovkaRepackResults)
 
-# fitgirlRepackResults = fitgirlRepackFilter.fitgirlSearch(search)
-# print(" ")
-# print(fitgirlRepackResults)
+fitgirlRepackResults = fitgirlRepackFilter.fitgirlSearch(search)
+print(" ")
+print(fitgirlRepackResults)
 
 
 

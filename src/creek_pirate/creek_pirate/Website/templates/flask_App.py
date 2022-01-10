b338b998708ec002456d95425c79468a2174c5c7
mystery_in_a_hoodie <53719487+ACE-OF-DIAMONDS@users.noreply.github.com>
2022-01-10 08:41:44 +0800

    .pyc file

--- src/creek_pirate/creek_pirate/Website/templates/flask_App.py
+++ src/creek_pirate/creek_pirate/Website/templates/flask_App.py
@@ -41,8 +41,7 @@ def settings():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=3900, debug=True)
 
 
 
